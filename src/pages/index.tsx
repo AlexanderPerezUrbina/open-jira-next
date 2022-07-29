8402b883fdb6b16860a59fba8a90760414afb4db
Alexander Perez Urbina <alexander.perez.trabajos@gmail.com>
2022-07-29 17:40:25 -0400

    [add] se agrega diseno del Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
-
 import { MainLayout } from '@/components/layouts';
+import { Card, CardHeader, Grid } from '@mui/material';
 
 const Home: NextPage = () => {
     return (
-        <MainLayout title="Home">
-            <Typography variant="h1" color="primary">
-                Hola mundo
-            </Typography>
+        <MainLayout title="Home | OpenJira">
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={4}>
+                    <Card sx={{ height: 'calc(100vh - 100px)' }}>
+                        <CardHeader title="Pendientes" />
+                    </Card>
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                    <Card sx={{ height: 'calc(100vh - 100px)' }}>
+                        <CardHeader title="En progreso" />
+                    </Card>
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                    <Card sx={{ height: 'calc(100vh - 100px)' }}>
+                        <CardHeader title="Completados" />
+                    </Card>
+                </Grid>
+            </Grid>
         </MainLayout>
     );
 };
